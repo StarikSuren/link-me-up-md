6796d681f3a8ccc022c2cbe7fee57f35d2c91f65
Starik Suren <stariksuren@yandex.ru>
2024-01-14 22:22:21 +0300

    UP: can now created links for yesterday REF: moved some code to functions, get some code refactored

--- main.ts
+++ main.ts
@@ -1,6 +1,60 @@
 import { Plugin } from 'obsidian'
 import { Command  } from 'obsidian';
 
+// date - supposed to be midnight of needed day
+function getFileListToLink(from: Date, to?: Date) {
+	console.log("from is: " + from);
+	console.log("to: is: " + to);
+	let createdFiles = [];
+	let modifiedFiles = [];
+
+	// Can be time consuming?
+	for (var file of this.app.vault.getMarkdownFiles()) {
+		if (to === undefined) {
+			console.log("to is undefined");
+			if (file.stat.ctime >= from) {
+				createdFiles.push(file.name);
+			}
+			if (file.stat.mtime >= from) {
+				modifiedFiles.push(file.name);
+			}
+		}
+		else {
+			if (file.stat.ctime >= from && file.stat.ctime <= to) {
+				createdFiles.push(file.name);
+			}
+			if (file.stat.mtime >= from && file.stat.mtime <= to) {
+				modifiedFiles.push(file.name);
+			}
+		}
+	}
+
+	return [createdFiles, modifiedFiles] as const;
+}
+
+function createLinks(createdFiles: any, modifiedFiles: any) {
+	var activeFile = this.app.workspace.getActiveFile();
+	// console.log("Files create today: ");
+	let payload = "# LinkMeUp" + '\n';
+	let linksToCreatedFiles = "## Files created:" + '\n'; 
+	for (var entry of createdFiles) {
+		// console.log(entry)
+		linksToCreatedFiles += "[[" + entry + "]]" + '\n';
+	}
+
+	let linksToModifiedFiles = "## Files modified:" + '\n'; 
+	for (var entry of modifiedFiles) {
+		// console.log(entry)
+		linksToModifiedFiles += "[[" + entry + "]]" + '\n';
+	}
+
+	payload += linksToCreatedFiles;
+	payload += linksToModifiedFiles;
+	
+	if (activeFile !== null) this.app.vault.append(activeFile, payload)
+	else console.log("Failed to find last active file. Try opening some file (like today's note)")
+}
+
 export default class LinkMeUpPlugin extends Plugin {
 	onload(): void {
 		this.addCommand({
@@ -10,44 +64,26 @@ export default class LinkMeUpPlugin extends Plugin {
 				// console.log("LinkMeUp: listing files...");
 				// console.log("| pathToFile | ctime | mtime |");
 				// console.log("| ---------- | ----- | ----- |");
-				let createdFiles = [];
-				let modifiedFiles = [];
-				let lastMidnight = new Date().setHours(0,0,0,0);
+				let lastMidnight = new Date(); 
+				lastMidnight.setHours(0,0,0,0);
 
-				// Can be time consuming?
-				for (var file of this.app.vault.getMarkdownFiles()) {
-					// console.log("| " + file.path + " | " + file.stat.ctime + " | " + file.stat.mtime + " |");
-					if (file.stat.ctime > lastMidnight) {
-						createdFiles.push(file.name);
-					}
-					else if (file.stat.mtime > lastMidnight) {
-						modifiedFiles.push(file.name);
-					}
-				}
-			
-				var activeFile = this.app.workspace.getActiveFile();
+				let [createdFiles,modifiedFiles] = getFileListToLink(lastMidnight);
+				createLinks(createdFiles, modifiedFiles);				
+			},
+		});
+		this.addCommand({
+			id: "lmu-list-yesterday",
+			name: "List files created or modified yesterday",
+			callback: () => {
+				let from = new Date();
+				from.setDate(from.getDate() - 1);	// that's for yesterday 00:00...
+				from.setHours(0,0,0,0);
 
-				// console.log("Files create today: ");
-				let payload = "# LinkMeUp" + '\n';
-				let linksToCreatedFiles = "## Files created today:" + '\n'; 
-				for (var entry of createdFiles) {
-					// console.log(entry)
-					linksToCreatedFiles += "[[" + entry + "]]" + '\n';
-				}
+				let to = new Date();				// ...that's for today's 00:00
+				to.setHours(0,0,0,0);
 
-				let linksToModifiedFiles = "## Files modified today:" + '\n'; 
-				for (var entry of modifiedFiles) {
-					// console.log(entry)
-					linksToModifiedFiles += "[[" + entry + "]]" + '\n';
-				}
-
-				payload += linksToCreatedFiles;
-				payload += linksToModifiedFiles;
-
-				
-				if (activeFile !== null) this.app.vault.append(activeFile, payload)
-				else console.log("Failed to find last active file. Try opening some file (like today's note)")
-
+				let [createdFiles, modifiedFiles] = getFileListToLink(from, to);
+				createLinks(createdFiles, modifiedFiles);
 			},
 		});
 	}
